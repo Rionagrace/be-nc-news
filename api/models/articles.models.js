e5d289c5d265c79eefa3fd57a2d8b358fb8b9161
riona mccumskay <r.mccumskay@gmail.com>
2024-10-14 15:47:02 +0100

    updated group by to be more concise

--- api/models/articles.models.js
+++ api/models/articles.models.js
@@ -32,13 +32,7 @@ INNER JOIN
 ON 
     articles.article_id = comments.article_id
 GROUP BY 
-    articles.author, 
-    articles.title, 
-    articles.article_id, 
-    articles.topic, 
-    articles.created_at, 
-    articles.votes, 
-    articles.article_img_url
+    articles.article_id 
 ORDER BY 
     articles.created_at DESC;`;
 
