a6b44e6918ede3d3c5a97bd359b4420437f2ae68
riona mccumskay <r.mccumskay@gmail.com>
2024-10-14 14:27:53 +0100

    finished 5 get-api-articles

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -71,3 +71,24 @@ describe("/api/articles/:article_id", () => {
     })
   })
 });
+
+describe("/api/articles", () => {
+  test("returns 200 and an array of all articles, descending by created_by", () => {
+    return request(app)
+    .get("/api/articles")
+    .expect(200)
+    .then(({body}) => {
+      console.log(body.articles)
+      expect(body.articles).toBeSortedBy("created_at",{descending: true})
+      body.articles.forEach((article) => {
+        expect(typeof article.author).toBe("string")
+        expect(typeof article.title).toBe("string")
+        expect(typeof article.article_id).toBe("number")
+        expect(typeof article.topic).toBe("string")
+        expect(typeof article.created_at).toBe("string")
+        expect(typeof article.votes).toBe("number")
+        expect(typeof article.article_img_url).toBe("string")
+      })
+    })
+  })
+})
