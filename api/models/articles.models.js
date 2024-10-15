59e97e71407410bc085630064c8948c2bd24fba8
riona mccumskay <r.mccumskay@gmail.com>
2024-10-15 16:12:41 +0100

    added comment count to get articles by id

--- api/models/articles.models.js
+++ api/models/articles.models.js
@@ -4,7 +4,24 @@ const { selectTopics, checkTopicExists } = require("./topics.models.js");
 
 function selectArticleById(article_id) {
 	return db
-		.query(`SELECT * FROM articles WHERE article_id = $1;`, [article_id])
+		.query(`SELECT 
+    articles.author, 
+    articles.title, 
+    articles.article_id, 
+    articles.topic, 
+		articles.body,
+    articles.created_at, 
+    articles.votes, 
+    articles.article_img_url, 
+    COUNT(comments.article_id) AS comment_count
+FROM 
+    articles
+LEFT JOIN 
+    comments 
+ON 
+    articles.article_id = comments.article_id
+		WHERE articles.article_id = $1 GROUP BY 
+    articles.article_id`, [article_id])
 		.then((result) => {
 			if (!result.rows.length) {
 				return Promise.reject({
@@ -12,6 +29,7 @@ function selectArticleById(article_id) {
 					msg: `Article not found`,
 				});
 			}
+			console.log(result.rows[0])
 			return result.rows[0];
 		});
 }
